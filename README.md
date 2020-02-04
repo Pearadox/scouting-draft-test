fa837a84bddc8b2271fb13c1e016863484212e3f
Alex Wong <awong5@slb.com>
2020-02-03 19:28:01 -0600

    add new function  getStudents

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
 # scouting-draft-test
+
 scouting draft
 
+#tech stack#
 
-tech stack
+1. firebase
+2. node
+3. firebase
 
-1) firebase
-2) node
-3) firebase 
+#setup#
+
+0. get the latest npm and node
+1. npm install -g firebase-tools
+1. firebase login
+1. firebase init
+1. npm install firebase-admin@latest firebase-functions@latest //get the latest code
+   firebase deploy
+
+https://console.firebase.google.com/u/0/project/scouting-draft-test/database/scouting-draft-test/data~2Fstudents
+https://us-central1-scouting-draft-test.cloudfunctions.net/helloWorld
+https://us-central1-scouting-draft-test.cloudfunctions.net/getStudent
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,28 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+admin.initializeApp();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  console.log("log-test as log");
+  console.warn("log-warn as log");
+  console.error("log-error as log");
+  response.send("Hello from FRC 5414.. we are going to win!!!!");
+});
+
+export const getStudent = functions.https.onRequest((request, response) => {
+  console.info("getStudent - begin");
+  admin
+    .firestore()
+    .collection("scouting-draft-test")
+    .doc("students")
+    .get()
+    .then(snapshot => {
+      response.send(snapshot.data());
+    })
+    .catch(error => {
+      response.status(500).send("something went wong!");
+    });
+});
